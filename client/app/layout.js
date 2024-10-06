a75619b8c65b4f9cd3d809defb66062eb8718e64
Anshu Kumar Singh <anshusinghak001@gmail.com>
2024-10-06 14:32:34 +0530

    initialized chakra

--- client/app/layout.js
+++ client/app/layout.js
@@ -1,6 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
-
+import { Providers } from "./providers";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -23,7 +23,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
