593338f66fdfb837b004910d57e1ada852ed3f7c
saeed <me.coderr@gmail.com>
2024-10-06 16:42:11 +0530

    using centralised db from current_app.db and finalised ruff as python linter/formatter extension

--- backend_svc/app/models/User.py
+++ backend_svc/app/models/User.py
@@ -1,25 +1,18 @@
-from pymongo import MongoClient
-from bson.json_util import dumps
 from flask import current_app
-# from app import db
 
-client = MongoClient("mongodb://localhost:27017/")
-db = client.CFC
+
 class UserModel:
     def __init__(self, user_document):
         self.user_id = user_document["user_id"]
         self.name = user_document["name"]
         self.email = user_document.get("email", None)
-    
+
     def toDict(self):
-        return {
-            "user_id": str(self.user_id),
-            "name": self.name,
-            "email": self.email
-        }
+        return {"user_id": str(self.user_id), "name": self.name, "email": self.email}
 
     @staticmethod
     def createUser(user_data):
+        db = current_app.db
         if db.users.find_one({"user_id": user_data["user_id"]}):
             return {"message": "User already exists"}, 409
         db.users.insert_one(user_data)
@@ -30,15 +23,21 @@ class UserModel:
         db = current_app.db
         user = db.users.find_one({"user_id": user_id})
         if user:
-            return UserModel(user).toDict() # Convert user document to JSON
-        return None  
+            return UserModel(user).toDict()
+        return None
 
     @staticmethod
     def updateUser(user_id, updated_data):
+        db = current_app.db
         result = db.users.update_one({"user_id": user_id}, {"$set": updated_data})
-        return {"message": "Updated successfully"} if result.matched_count else {"message": "User not found"}, 404
+        return {"message": "Updated successfully"} if result.matched_count else {
+            "message": "User not found"
+        }, 404
 
     @staticmethod
     def deleteUser(user_id):
+        db = current_app.db
         result = db.users.delete_one({"user_id": user_id})
-        return {"message": "Deleted successfully"} if result.deleted_count else {"message": "User not found"}, 404
+        return {"message": "Deleted successfully"} if result.deleted_count else {
+            "message": "User not found"
+        }, 404
