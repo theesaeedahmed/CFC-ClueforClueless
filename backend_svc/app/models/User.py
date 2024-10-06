ba35c7871bf2ed6cefad8257800a2554c6ce6539
saeed <me.coderr@gmail.com>
2024-10-06 16:02:09 +0530

    using current_app to access db from centralised app

--- backend_svc/app/models/User.py
+++ backend_svc/app/models/User.py
@@ -1,35 +1,44 @@
 from pymongo import MongoClient
 from bson.json_util import dumps
+from flask import current_app
 # from app import db
 
 client = MongoClient("mongodb://localhost:27017/")
 db = client.CFC
 class UserModel:
-    def __init__(self, user_id, name, email=None):
-        self.user_id = user_id
-        self.name = name
-        self.email = email
+    def __init__(self, user_document):
+        self.user_id = user_document["user_id"]
+        self.name = user_document["name"]
+        self.email = user_document.get("email", None)
+    
+    def toDict(self):
+        return {
+            "user_id": str(self.user_id),
+            "name": self.name,
+            "email": self.email
+        }
 
     @staticmethod
-    def create_user(user_data):
+    def createUser(user_data):
         if db.users.find_one({"user_id": user_data["user_id"]}):
             return {"message": "User already exists"}, 409
         db.users.insert_one(user_data)
         return {"message": "User created successfully"}, 201
 
     @staticmethod
-    def find_user_by_id(user_id):
+    def findUserById(user_id):
+        db = current_app.db
         user = db.users.find_one({"user_id": user_id})
         if user:
-            return dumps(user) # Convert user document to JSON
+            return UserModel(user).toDict() # Convert user document to JSON
         return None  
 
     @staticmethod
-    def update_user(user_id, updated_data):
+    def updateUser(user_id, updated_data):
         result = db.users.update_one({"user_id": user_id}, {"$set": updated_data})
         return {"message": "Updated successfully"} if result.matched_count else {"message": "User not found"}, 404
 
     @staticmethod
-    def delete_user(user_id):
+    def deleteUser(user_id):
         result = db.users.delete_one({"user_id": user_id})
         return {"message": "Deleted successfully"} if result.deleted_count else {"message": "User not found"}, 404
--- backend_svc/app/resources/User.py
+++ backend_svc/app/resources/User.py
@@ -4,11 +4,7 @@ from app.models import UserModel
 
 class User(Resource):
     def get(self, user_id):
-        response = UserModel.find_user_by_id(user_id)
-        # if user:
-        #     return {"user_id": user["user_id"], "name": user["name"], "email": user.get("email")}, 200
-        # else:
-        #     return {"message": "User not found"}, 404
+        response = UserModel.findUserById(user_id)
         return response
 
     def post(self, user_id):
@@ -16,14 +12,14 @@ class User(Resource):
         if not data or "user_id" not in data or "name" not in data:
             return {"message": "Bad request, missing user_id or name"}, 400
 
-        response = UserModel.create_user(data)
+        response = UserModel.createUser(data)
         return response
 
     def put(self, user_id):
         data = request.get_json()
-        response = UserModel.update_user(user_id, data)
+        response = UserModel.updateUser(user_id, data)
         return response
 
     def delete(self, user_id):
-        response = UserModel.delete_user(user_id)
+        response = UserModel.deleteUser(user_id)
         return response
