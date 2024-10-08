f97873cf6ac75532159debc26425c5c4ab5b66d4
saeed <me.coderr@gmail.com>
2024-10-09 02:48:12 +0530

    basic stuff - checking repo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,11 @@
-import { Routes,Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
-
     <Routes>
-      <Route path="/" element={<div>Home page</div>}/>
-      <Route path="/login" element={<div>Home login page</div>}/>
-      <Route path="/signup" element={<div>Home page</div>}/>
+      <Route path="/" element={<div>Home page</div>} />
+      <Route path="/login" element={<div>Home login page</div>} />
+      <Route path="/signup" element={<div>Home signup page</div>} />
     </Routes>
-  )
+  );
 }
-
-
