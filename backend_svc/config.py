fa94bedef8975023341ac1d6845d51ddfd45008f
saeed <me.coderr@gmail.com>
2024-10-09 03:02:16 +0530

    formatting change

--- backend_svc/config.py
+++ backend_svc/config.py
@@ -4,7 +4,8 @@ import os
 # Load environment variables from .env file
 load_dotenv()
 
+
 class Config:
     MONGO_URI = os.getenv("MONGO_URI_PROD") if os.getenv("IS_PROD") == "True" else os.getenv("MONGO_URI_LOCAL", "mongodb://localhost:27017/")
     DB_NAME = os.getenv("DB_NAME", "CFC")
-    DEBUG = False if os.getenv("IS_Prod") == "True" else True
+    DEBUG = False if os.getenv("IS_PROD") == "True" else True
