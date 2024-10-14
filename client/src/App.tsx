1e8f43afef2debe5b853df611b0458b42ae4336b
anchal0914 <anchalg1409@gmail.com>
2024-10-14 15:41:16 +0530

    half login page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes,Route } from "react-router-dom"
+import Login from "./pages/Login"
+
 
 export default function App() {
   return (
 
     <Routes>
       <Route path="/" element={<div>Home page</div>}/>
-      <Route path="/login" element={<div>Home login page</div>}/>
+      <Route path="/login" element={<Login/>}/>
       <Route path="/signup" element={<div>Home page</div>}/>
     </Routes>
   )
